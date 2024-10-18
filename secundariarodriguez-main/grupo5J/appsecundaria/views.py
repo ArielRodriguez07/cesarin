35a1bb190f8b7567254f7b1b11b0954c813efe6e
ArielRodriguez07 <a.22308051280706@cbtis128.edu.mx>
2024-10-18 09:54:33 -0600

    Add files via upload

---
+++ secundariarodriguez-main/grupo5J/appsecundaria/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from.models import AlumnoT,Frase
+
+# Create your views here.
+def index_vista(request):
+    objeto=AlumnoT.objects.all().order_by( "id")#nuevo mas el diccionario
+    return render(request,"index.html",{"objeto":objeto})
